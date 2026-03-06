abce0d51b4d5f1387827dd47c9f94c66ebb20473
sahildatkhile25 <sahildatkhile25@gmail.com>
2026-03-06 21:49:01 +0530

    Add Student class with id and name attributes

---
+++ main.java
@@ -0,0 +1,11 @@
+class Student {
+    int id;
+    String name;
+    Student next;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.next = null;
+    }
+}
